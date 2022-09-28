4396d3117c05269ed60fb1da861d4712ccc83d56
satania-lee <rockky.tl@gmail.com>
2022-09-28 19:51:51 +0300

    002 task 1 complete

---
+++ 002-console/task1/date.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+const today = new Date();
+
+const isoDate = today.toISOString();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+const time = isoDate.slice(10);
+
+const argvObj = yargs(hideBin(process.argv)).parse();
+
+const argv = yargs(hideBin(process.argv))
+  .option('year', {
+    alias: 'y',
+    describe: 'Show current year',
+  })
+  .option('month', {
+    alias: 'm',
+    describe: 'Show current month',
+  })
+  .option('date', {
+    alias: 'd',
+    describe: 'Show current date',
+  })
+  .command({
+    command: 'add',
+    describe: 'Add smth',
+    handler() {
+      if (argvObj['d'] || argvObj['date']) {
+        const newDay = date + (argvObj['d'] || argvObj['date']);
+        const newDate = new Date(year, month - 1, newDay + 1);
+        console.log(newDate.toISOString().slice(0, 10) + time);
+      }
+      if (argvObj['m'] || argvObj['month']) {
+        const newMonth = month + (argvObj['m'] || argvObj['month']);
+        const newDate = new Date(year, newMonth - 1, date + 1);
+        console.log(newDate.toISOString().slice(0, 10) + time);
+      }
+    },
+  })
+  .command({
+    command: 'sub',
+    describe: 'Substract smth',
+    handler() {
+      if (argvObj['d'] || argvObj['date']) {
+        const newDay = date - (argvObj['d'] || argvObj['date']);
+        const newDate = new Date(year, month - 1, newDay + 1);
+        console.log(newDate.toISOString().slice(0, 10) + time);
+      }
+      if (argvObj['m'] || argvObj['month']) {
+        const newMonth = month - (argvObj['m'] || argvObj['month']);
+        const newDate = new Date(year, newMonth - 1, date + 1);
+        console.log(newDate.toISOString().slice(0, 10) + time);
+      }
+    },
+  })
+  .parse();
+
+if (argv._.length === 0) {
+  if (argv.year) {
+    console.log(year);
+  } else if (argv.month) {
+    console.log(month);
+  } else if (argv.date) {
+    console.log(date);
+  } else {
+    console.log(isoDate);
+  }
+}
